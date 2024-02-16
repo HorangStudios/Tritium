a29650837d5c41c582db73e4912a31fad86bb6a0
Bobuxstation <91587018+Bobuxstation@users.noreply.github.com>
2024-02-16 18:35:14 +0700

    update

--- src/main.js
+++ src/main.js
@@ -19,4 +19,23 @@ window.addEventListener("DOMContentLoaded", () => {
       })
     );
 
+  fetch("https://horangstudios.github.io/HorangStudios-Roblox-Games-API/")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) =>
+      data.items.forEach(items => {
+        let gameList = document.getElementById("cards");
+        let btn = document.createElement("div");
+        let gamename = "<h4>" + items.name + "</h4>";
+        btn.innerHTML = gamename;
+        btn.className = "card";
+        let banner = items.banner;
+        btn.style.backgroundImage = "url(" + banner + ")";
+        btn.onclick = () => {
+          window.location.replace(`roblox://placeId=${items.gameid}/`)
+        };
+        gameList.appendChild(btn);
+      })
+    );
 });
